e34e3305d12fcbd740950c3425977195bb2bbf45
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2022-04-03 13:20:47 +0530

    styling for buttons and list box

--- words/src/app/body/body.component.ts
+++ words/src/app/body/body.component.ts
@@ -18,14 +18,6 @@ export class BodyComponent implements OnInit {
     this.words = [
       {name: 'Australia', code: 'AU'},
       {name: 'Brazil', code: 'BR'},
-      {name: 'China', code: 'CN'},
-      {name: 'Egypt', code: 'EG'},
-      {name: 'France', code: 'FR'},
-      {name: 'Germany', code: 'DE'},
-      {name: 'India', code: 'IN'},
-      {name: 'Japan', code: 'JP'},
-      {name: 'Spain', code: 'ES'},
-      {name: 'United States', code: 'US'}
   ];
 
   }
